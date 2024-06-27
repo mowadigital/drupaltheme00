2bd6d3b1ea83c4c0dbf3e03bb8e0c8b8e257ff94
Oscar Lee <oscarlee@Oscars-MacBook-Pro.local>
2024-06-27 12:56:54 -0400

    Lorem ipsum

--- app.js
+++ app.js
@@ -17,3 +17,87 @@ i000.forEach(item => {
     item.style.setProperty("--y", "50%")
   })
 })
+
+/*
+document.addEventListener("mousemove", (e) => {
+  document.querySelectorAll(".imagetransform").forEach((i) => {
+    const position = i.getAttribute("value")
+    const x = (window.innerWidth - e.pageX * position) / 45
+    const y = (window.innerHeight - e.pageY * position) / 45
+
+    i.style.transform = `translate(${ x }px, ${ y }px)`
+  })
+})
+*/
+
+function handleCarousel() {
+  let carousel = document.getElementById("carousel")
+  let tracker = 0
+
+  setInterval(() => {
+    tracker++
+
+    if (tracker > 3) tracker = 0
+
+    for (let i = 0; i < document.getElementById("carouselcontroller").children.length; i++) {
+      document.getElementById("carouselcontroller").children[i].setAttribute("active", "false")
+    }
+    document.getElementById("carouselcontroller").children[tracker].setAttribute("active", "true")
+
+    switch(tracker) {
+      case 0:
+        carousel.style.setProperty("-webkit-transform", "translateX(0%)")
+        carousel.style.setProperty("transform", "translateX(0%)")
+        break
+      case 1:
+        carousel.style.setProperty("-webkit-transform", "translateX(-100%)")
+        carousel.style.setProperty("transform", "translateX(-100%)")
+        break
+      case 2:
+        carousel.style.setProperty("-webkit-transform", "translateX(-200%)")
+        carousel.style.setProperty("transform", "translateX(-200%)")
+        break
+      case 3:
+        carousel.style.setProperty("-webkit-transform", "translateX(-300%)")
+        carousel.style.setProperty("transform", "translateX(-300%)")
+        break
+      default:
+        return
+    }
+  }, 3000)
+
+  document.addEventListener("click", (e) => {
+    if (!e.target.hasAttribute("carouselvalue")) {
+      return
+    } else {
+      for (let i = 0; i < document.getElementById("carouselcontroller").children.length; i++) {
+        document.getElementById("carouselcontroller").children[i].setAttribute("active", "false")
+      }
+      e.target.setAttribute("active", "true")
+
+      let carousel = document.getElementById("carousel")
+      switch(e.target.getAttribute("carouselvalue")) {
+        case "0":
+          carousel.style.setProperty("-webkit-transform", "translateX(0%)")
+          carousel.style.setProperty("transform", "translateX(0%)")
+          break
+        case "1":
+          carousel.style.setProperty("-webkit-transform", "translateX(-100%)")
+          carousel.style.setProperty("transform", "translateX(-100%)")
+          break
+        case "2":
+          carousel.style.setProperty("-webkit-transform", "translateX(-200%)")
+          carousel.style.setProperty("transform", "translateX(-200%)")
+          break
+        case "3":
+          carousel.style.setProperty("-webkit-transform", "translateX(-300%)")
+          carousel.style.setProperty("transform", "translateX(-300%)")
+          break
+        default:
+          return
+      }
+    }
+  })
+}
+
+handleCarousel()
